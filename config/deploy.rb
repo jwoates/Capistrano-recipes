1b9544a1e5ddae13dfbcf7b868479524b5cdfb9f
jackson <jackson@roundhouseagency.com>
2013-04-24 14:05:03 -0700

    added example env config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,11 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# require 'bundler/setup'
+# require 'bundler/capistrano'
+# require 'hipchat/capistrano'
+
+set :scm, "git"
+set :repository, "git@github.com:allibubba/Capistrano-recipes.git"
+set :origin, "origin"
+set :branch, "deploy"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -9,8 +15,9 @@ role :app, "your app-server here"                          # This may be the sam
 role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 role :db,  "your slave db-server here"
 
+
 # user the application will run under
-set :system_user, ''
+set :system_user, 'roundhouse'
 
 # add basic http authentication? if true specify password.
 set :http_auth, true
