42498fe86c0f75a75ad8ba2f141cf054e55cd775
jackson <jackson@roundhouseagency.com>
2013-04-24 13:55:51 -0700

    capified

---
+++ config/deploy.rb
@@ -0,0 +1,17 @@
+set :application, "set your application name here"
+set :repository,  "set your repository location here"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+role :app, "your app-server here"                          # This may be the same as your `Web` server
+role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :db,  "your slave db-server here"
+
+# user the application will run under
+set :system_user, ''
+
+# add basic http authentication? if true specify password.
+set :http_auth, true
+set :http_auth_password, ''
